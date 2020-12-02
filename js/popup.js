b6abe31da042f2a832b0153f3dc34a96be23d291
tiagorfilipe22 <tiagorfilipe22@hotmail.com>
2020-12-02 23:22:24 +0000

    update

---
+++ js/popup.js
@@ -0,0 +1,56 @@
+// When the user clicks on div, open the popup
+	function myFunction1() {
+	  var popup = document.getElementById("myPopup1");
+	  popup.classList.toggle("show");
+	}
+
+
+	function myFunction2() {
+	  var popup = document.getElementById("myPopup2");
+	  popup.classList.toggle("show");
+	}
+
+	function myFunction3() {
+	  var popup = document.getElementById("myPopup4");
+	  popup.classList.toggle("show");
+	}
+
+	function myFunction4() {
+	  var popup = document.getElementById("myPopup4");
+	  popup.classList.toggle("show");
+	}
+
+	function myFunction5() {
+	  var popup = document.getElementById("myPopup5");
+	  popup.classList.toggle("show");
+	}
+
+	function myFunction6() {
+	  var popup = document.getElementById("myPopup6");
+	  popup.classList.toggle("show");
+	}
+
+	function myFunction7() {
+	  var popup = document.getElementById("myPopup7");
+	  popup.classList.toggle("show");
+	}
+
+	function myFunction8() {
+	  var popup = document.getElementById("myPopup8");
+	  popup.classList.toggle("show");
+	}
+
+	function myFunction9() {
+	  var popup = document.getElementById("myPopup9");
+	  popup.classList.toggle("show");
+	}
+
+	function myFunction10() {
+	  var popup = document.getElementById("myPopup10");
+	  popup.classList.toggle("show");
+	}
+
+	function myFunction11() {
+	  var popup = document.getElementById("myPopup11");
+	  popup.classList.toggle("show");
+	}
